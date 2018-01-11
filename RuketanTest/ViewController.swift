059f2a51bbe5aeaf5297870cac984dae0a9d96d1
aaleemakhund <aaleemakhund@yahoo.com>
2018-01-11 22:46:48 +0800

    Initial Commit

---
+++ RuketanTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RuketanTest
+//
+//  Created by Shauket on 1/11/18.
+//  Copyright © 2018 Shauket. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
